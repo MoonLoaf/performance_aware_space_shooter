a302a80dcdfdb5529f83e7f8e1ad26ec339a5fb6
MoonLoaf <oskarw.99@hotmail.com>
2023-10-09 15:15:25 +0200

    Creating a blank window
    - creating a window
    - adding libraries to project and linking through build.rs instead of locally

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,37 @@
-use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+
 use std::time::Duration;
 
 fn main() -> Result<(), String> {
+    println!("Starting");
+
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let _window = video_subsystem.window("Space Shooter | Oskar Wistedt", 800, 600)
         .position_centered()
         .build()
-        .expect("could not initialize video subsystem");
+        .expect("Could not init video subsystem");
 
-    let mut canvas = window.into_canvas().build()
-        .expect("could not make a canvas");
-
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
-    let mut i = 0;
-    'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+
+    'running:loop {
         for event in event_pump.poll_iter() {
+
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+
+                Event::Quit {..} =>{
+                    break 'running;
+                },
+                Event::KeyDown {keycode: Some(Keycode::Escape), .. } => {
                     break 'running;
                 },
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
-
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        //Cap the event pump loop to run 60 times per second
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32/60));
     }
-
-    Ok(())
+    return Ok(())
 }
